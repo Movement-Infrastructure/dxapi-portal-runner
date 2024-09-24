364585fc2b6467ad1a35ddd4108ea6a32b6e205e
Anna Armentrout <aarmentrout@schmidtfutures.com>
2024-09-24 17:13:44 -0400

    use the private key var

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from mig_dx_api import DX
 
 private_key = f"-----BEGIN PRIVATE KEY-----\n{os.environ.get("PRIVATE_KEY")}\n-----END PRIVATE KEY-----"
 # Initialize the client
-dx = DX(app_id=os.environ.get("APP_ID"), private_key=os.environ.get("PRIVATE_KEY"))
+dx = DX(app_id=os.environ.get("APP_ID"), private_key=private_key)
 dx.base_url = os.environ.get("BASE_URL")
 
 user_info = dx.whoami()
