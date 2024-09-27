d23ee061a09d15b926c2b6e3d19d9d6a17d16c08
Anna Armentrout <aarmentrout@schmidtfutures.com>
2024-09-27 18:55:37 -0400

    merge and reorganize code

--- run.py
+++ run.py
@@ -28,28 +28,13 @@ with dx.installation(installation) as ctx:
     dataset_ops = ctx.datasets.find(name=table_name)
     if dataset_ops:
         print(f'Found dataset with name {table_name}. Updating...')
+        # Get data from source dataset
+        # Get signed uploadurl from MIG
+        # Upload the data for the dataset in MIG to presigned url
+        # Log results
     else:
         print(f'Did not find dataset with name {table_name}. Creating...')
 
+# If dataset doesn't exist, get schema of source table and create MIG dataset
 def create_dataset():
-
-# If dataset doesn't exist,
-    # get schema of source table and create MIG dataset
-# Get data from source dataset
-# Get signed uploadurl from MIG
-# Upload the data for the dataset in MIG to presigned url
-# Log results
-
-# Pass in name of BigQuery dataset from ScriptRunner
-dataset_id = sys.argv[1]
-table_name = sys.argv[2]
-
-print(f"dataset_id {dataset_id} and table_name {table_name}")
-
-# Check if dataset of specified name already exists
-# If dataset doesn't exist,
-    # get schema of source table and create MIG dataset
-# Get data from source dataset
-# Get signed uploadurl from MIG
-# Upload the data for the dataset in MIG to presigned url
-# Log results
+    return
