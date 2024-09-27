5e581fa9d1e04b47ae4cf6c4ca7fe1e33c5607d5
Anna Armentrout <atrout@users.noreply.github.com>
2024-09-27 17:29:58 -0400

    Pass parameters from ScriptRunner to MIG processing code (#3)
    * updated requirements
    
    * pass BigQuery parameters from ScriptRunner
    
    * alphabetize requirements

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from mig_dx_api import DX
 
 private_key = f"-----BEGIN PRIVATE KEY-----\n{os.environ.get("PRIVATE_KEY")}\n-----END PRIVATE KEY-----"
@@ -8,3 +9,17 @@ dx.base_url = os.environ.get("BASE_URL")
 
 user_info = dx.whoami()
 print(f"user info: {user_info}\n")
+
+# Pass in name of BigQuery dataset from ScriptRunner
+dataset_id = sys.argv[1]
+table_name = sys.argv[2]
+
+print(f"dataset_id {dataset_id} and table_name {table_name}")
+
+# Check if dataset of specified name already exists
+# If dataset doesn't exist,
+    # get schema of source table and create MIG dataset
+# Get data from source dataset
+# Get signed uploadurl from MIG
+# Upload the data for the dataset in MIG to presigned url
+# Log results
--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 # Paste this code into the `startup command` text input box in portal
 pip install -r requirements.txt
-python run.py
+python run.py {{dataset_id}} {{table_name}}
